21efcd312d503b8713166e6afdd2f25dc64d2731
Gaurav Mehta <gaurav.mehta@iag.com.au>
2017-02-24 17:11:29 +1100

    fixed main.go

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import "github.com/ibrokethecloud/go-eureka-client"
 import "github.com/ibrokethecloud/go-utils"
 import "encoding/json"
 
+
 func main() {
   var JsonRegistrationInfo,err = json.Marshal(eureka.DefaultRegisterationInfo)
   utils.CheckError(err)
