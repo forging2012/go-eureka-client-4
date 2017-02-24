2792c4c04572f3377c6e041bd45fa16ba3671f37
Gaurav Mehta <gaurav.mehta@iag.com.au>
2017-02-24 17:45:26 +1100

    added register function

---
+++ eureka/register.go
@@ -0,0 +1,36 @@
+package eureka
+
+import "github.com/ibrokethecloud/go-utils"
+import "net/http"
+import "time"
+import  log "github.com/Sirupsen/logrus"
+// Function to register with Eureka //
+
+func RegisterClient(registerationInfo []byte){
+  // Call Eureka end point and register //
+
+  resp_code := 000
+
+  for resp_code != 204 {
+
+    req, err := http.NewRequest("POST", EUREKA_ENDPOINT, bytes.NewBuffer(jsonStr))
+    req.Header.Set("Content-Type", "application/json")
+    client := &http.Client{}
+    resp, err := client.Do(req)
+
+    utils.CheckError(err)
+
+    defer resp.Body.Close()
+    log.Infof("response Status: %v", resp.Status)
+    log.Infof("response Headers: %v", resp.Header)
+
+    body, _ := ioutil.ReadAll(resp.Body)
+    log.Infof("response Body: %s", string(body))
+
+    resp_code = resp.Status
+
+     time.Sleep(time.Second * 10)
+
+  }
+
+}
--- main.go
+++ main.go
@@ -1,13 +1,15 @@
 package main
 
-import "fmt"
+import "os"
 import "github.com/ibrokethecloud/go-eureka-client/eureka"
 import "github.com/ibrokethecloud/go-utils"
 import "encoding/json"
 
+var EUREKA_ENDPOINT = os.Getenv("EUREKA_ENDPOINT")
 
 func main() {
   var JsonRegistrationInfo,err = json.Marshal(eureka.DefaultRegisterationInfo)
   utils.CheckError(err)
-  fmt.Println(string(JsonRegistrationInfo))
+
+  eureka.RegisterClient(JsonRegistrationInfo)
 }
