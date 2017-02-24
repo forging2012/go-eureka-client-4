2797071a609ed08e03e2243bb58ce2323a5d6ca0
Gaurav Mehta <gaurav.mehta@iag.com.au>
2017-02-24 17:15:32 +1100

    Renamed type to eureka

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import "fmt"
-import "github.com/ibrokethecloud/go-eureka-client"
+import "github.com/ibrokethecloud/go-eureka-client/type"
 import "github.com/ibrokethecloud/go-utils"
 import "encoding/json"
 
